c53a13f8544ae791ace82067c0c651322ceb2951
suparnav74 <suparnav74@gmail.com>
2024-10-13 15:08:03 +0530

    Save data using mongoose model

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    Obj ={
-        a : 'suparna',
-        number : 30
-    }
-    res.json(Obj)
+// Create a User using: POST "/api/auth". Does not require Auth
+router.post('/',(req,res)=>{
+    
+    console.log(req.body);
+    
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
+
 })
 
 module.exports = router

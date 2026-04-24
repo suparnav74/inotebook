6143016faf7a57f6bf2849eba42e11019f4b3ffb
suparnav74 <suparnav74@gmail.com>
2026-04-24 17:40:12 +0530

    fix cors for vercel frontend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
+app.use(cors({
+  origin: "https://inotebook-lake-theta.vercel.app/",
+  credentials: true
+}));
+
 
 //Avialable Routes
 app.use('/api/auth',require('./routes/auth'))

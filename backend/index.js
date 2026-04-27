8ed4bfd7943a06ace9fd7dce3bb3ab431a575255
suparnav74 <suparnav74@gmail.com>
2026-04-27 17:19:38 +0530

    fixed cors trailing slash issue

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 //app.use(cors())
 app.use(cors({
-  origin: "https://inotebook-lake-theta.vercel.app/",
+  origin: "https://inotebook-lake-theta.vercel.app",
   credentials: true
 }));
 
